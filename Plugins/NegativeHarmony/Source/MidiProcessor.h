f4048cb603cb4e8b9a13e0971bace9419e648907
Philipp Seeger <philippbseeger@googlemail.com>
2020-06-01 15:12:14 +0200

    Negative Harmony Midi Effect

---
+++ Plugins/NegativeHarmony/Source/MidiProcessor.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+class MidiProcessor : AudioProcessorValueTreeState::Listener
+{
+public:
+    explicit MidiProcessor (AudioProcessorValueTreeState& vts);
+    ~MidiProcessor() override;
+
+    void processMidiMsgsBlock (MidiBuffer& midi_messages);
+
+private:
+    void parameterChanged (const String& parameter_id, float new_value) override;
+
+    int getNegHarmNn (int nn);
+
+    MidiBuffer p_midi_buffer_;
+
+    AudioProcessorValueTreeState& apvts_;
+
+    std::atomic<float>* is_on_;
+    std::atomic<float>* cur_key_;
+
+    bool state_changed_;
+};
